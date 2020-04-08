2525f0f985f209c7fbca3353014a76c44e2c14b8
rphillips <rp@aapi.io>
2020-04-07 18:58:03 -0700

    Added MPH calc for timestamps and other calculations for distance

--- main.go
+++ main.go
@@ -1,32 +1,43 @@
 package main
 
 import (
-	"fmt"
-	"github.com/oschwald/geoip2-golang"
+	"github.com/aapi-rp/geo-velocity/utils"
 	"log"
-	"net"
 )
 
 func main() {
-	db, err := geoip2.Open("db/GeoLite2-City.mmdb")
+	geoP, err := utils.GetGeoDataFromIP("91.207.175.104")
+
 	if err != nil {
 		log.Println(err)
-	}
-	defer db.Close()
-	// If you are using strings that may be invalid, check that ip is not nil
-	//67.181.148.192
-	ip := net.ParseIP("72.208.39.180")
-	record, err := db.City(ip)
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Printf("City name: %v\n", record.City.Names["en"])
-	if len(record.Subdivisions) > 0 {
-		fmt.Printf("English subdivision name: %v\n", record.Subdivisions[0].Names["en"])
 	}
 
-	fmt.Printf("ISO country code: %v\n", record.Country.IsoCode)
-	fmt.Printf("Time zone: %v\n", record.Location.TimeZone)
-	fmt.Printf("Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)
+	log.Println(geoP.LAT, geoP.LONG, geoP.CITY, geoP.SUBDEVISIONNAME)
+
+	curr := utils.Coord{}
+
+	curr.Longitude = geoP.LONG
+	curr.Latitude = geoP.LAT
+
+	geoC, err := utils.GetGeoDataFromIP("206.81.252.6")
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println(geoC.LAT, geoC.LONG, geoC.CITY, geoC.SUBDEVISIONNAME)
+
+	pre := utils.Coord{}
+
+	pre.Longitude = geoC.LONG
+	pre.Latitude = geoC.LAT
+
+	vdist := utils.VariableDistance(geoP.LAT, geoP.LONG, geoC.LAT, geoC.LONG, "miles")
+
+	log.Println("Variable Distance: ", vdist)
+
+	mph := utils.MPH(vdist, 1514851200, 1514764800)
+
+	log.Println("Miles Per Hour: ", mph)
 
 }
--- utils/toolbox.go
+++ utils/toolbox.go
@@ -1,7 +1,10 @@
 package utils
 
 import (
+	"github.com/oschwald/geoip2-golang"
+	"log"
 	"math"
+	"net"
 	"time"
 )
 
@@ -11,23 +14,108 @@ func getCurrentEpochTime() time.Time {
 	return time.Unix(secs, 0)
 }
 
-type Coordinates struct {
+type Coord struct {
 	Latitude  float64
 	Longitude float64
 }
 
-const radius = 6371 // Earth's avarage radius in kilometers
-
-func deg2radians(degrees float64) float64 {
-	return degrees * math.Pi / 180
+type GeoData struct {
+	UUID            []byte
+	LOGIN_TIME      int64
+	USERNAME        string
+	IP_ADDRESS      string
+	LAT             float64
+	LONG            float64
+	RADIUS          uint16
+	ISOCountryCode  string
+	TIMEZONE        string
+	SUBDEVISIONNAME string
+	CITY            string
 }
 
-func Distance(origin Coordinates, destination Coordinates) float64 {
-	dgLat := deg2radians(destination.Latitude - origin.Latitude)
-	dgLong := deg2radians(destination.Longitude - origin.Longitude)
-	a := math.Sin(dgLat/2)*math.Sin(dgLong/2) + math.Cos(deg2radians(origin.Latitude))*math.Cos(deg2radians(destination.Latitude))*math.Sin(dgLong/2)*math.Sin(dgLong/2)
-	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
-	d := radius * c
+func GetGeoDataFromIP(ipaddr string) (GeoData, error) {
 
-	return d
+	Geo := GeoData{}
+
+	db, err := geoip2.Open("db/GeoLite2-City.mmdb")
+	if err != nil {
+		log.Println(err)
+		return Geo, err
+	}
+	defer db.Close()
+
+	//67.181.148.192
+	ip := net.ParseIP(ipaddr)
+	record, err := db.City(ip)
+	if err != nil {
+		log.Println(err)
+		return Geo, err
+	}
+	Geo.CITY = record.City.Names["en"]
+	if len(record.Subdivisions) > 0 {
+		Geo.SUBDEVISIONNAME = record.Subdivisions[0].Names["en"]
+	}
+
+	Geo.IP_ADDRESS = ipaddr
+	Geo.LAT = record.Location.Latitude
+	Geo.LONG = record.Location.Longitude
+	Geo.RADIUS = record.Location.AccuracyRadius
+
+	return Geo, nil
+
+}
+
+// Some of the below methods I got from gist.github.com/cdipaolo and some I wrote myself
+// The part I wrote myself was the different types of measurement conversions
+// I did this so I could test the method against google for accuracy.
+
+func hsin(theta float64) float64 {
+	return math.Pow(math.Sin(theta/2), 2)
+}
+
+func VariableDistance(lat1, lon1, lat2, lon2 float64, conversiontype string) float64 {
+	// convert to radians
+	// must cast radius as float to multiply later
+	var la1, lo1, la2, lo2, r float64
+	la1 = lat1 * math.Pi / 180
+	lo1 = lon1 * math.Pi / 180
+	la2 = lat2 * math.Pi / 180
+	lo2 = lon2 * math.Pi / 180
+
+	r = 6378100 // Earth radius in METERS
+
+	// calculate
+	h := hsin(la2-la1) + math.Cos(la1)*math.Cos(la2)*hsin(lo2-lo1)
+	g := 2 * r * math.Asin(math.Sqrt(h))
+
+	if conversiontype == "meters" {
+		log.Println("meters")
+		return g
+	}
+
+	if conversiontype == "miles" {
+		log.Println("miles")
+		c := g / 1609
+		return c
+	}
+
+	if conversiontype == "kilometers" {
+		log.Println("kilometers")
+		c := g / 1000
+		return c
+	}
+
+	return g
+}
+
+func MPH(distance float64, startTime, endTime int64) int {
+
+	st := time.Unix(startTime, 0)
+	et := time.Unix(endTime, 0)
+	// Get difference in times in hours from the time stamps above
+	hrs := et.Sub(st).Hours()
+	// Divide distance by absolute value of hours
+	sp := distance / math.Abs(hrs)
+
+	return int(sp)
 }
